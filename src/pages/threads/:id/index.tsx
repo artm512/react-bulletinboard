594b0b7da289778c694436b067946c22f625b809
artm512 <44886239+artm512@users.noreply.github.com>
2024-11-11 17:41:58 +0900

    feat: post new text

--- src/pages/threads/:id/index.tsx
+++ src/pages/threads/:id/index.tsx
@@ -1,11 +1,14 @@
 import { FC, memo, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 // TODO: styled-systemへの参照をエイリアス化したい
 import { css } from "../../../../styled-system/css";
 import { Stack } from "../../../../styled-system/jsx";
 import { Heading } from "../../../components/ui/heading";
 import { Card } from "../../../components/ui/card";
+import { Textarea } from "../../../components/ui/textarea";
+import { Button } from "../../../components/ui/button";
 
 import { type TThreads } from "../../../App";
 
@@ -28,6 +31,15 @@ const postStyles = css({
   },
 });
 
+const errorMessageStyles = css({
+  marginTop: "2",
+  color: "red",
+});
+
+const textareaStyles = css({
+  height: "40",
+});
+
 // TODO: 共通化する
 const baseUrl = "https://railway.bulletinboard.techtrain.dev";
 
@@ -36,9 +48,20 @@ type TPosts = {
   post: string;
 }[];
 
+type Inputs = {
+  post_text: string;
+};
+
 export const ThreadsId: FC = memo(() => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<Inputs>();
   const params = useParams();
   const { id } = params;
+  const [isLoading, setIsLoading] = useState(false);
   const [threadTitle, setThreadTitle] = useState<string>("");
   const [posts, setPosts] = useState<TPosts>([]);
 
@@ -74,6 +97,30 @@ export const ThreadsId: FC = memo(() => {
     }
   };
 
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { post_text } = data;
+
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(`${baseUrl}/threads/${id}/posts`, {
+        method: "POST",
+        body: JSON.stringify({ post: post_text }),
+      });
+      if (!response.ok) {
+        throw new Error(`レスポンスステータス: ${response.status}`);
+      }
+      await response.json();
+      setValue("post_text", "");
+
+      fetchThreadPosts();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
     fetchThreadTitle();
     fetchThreadPosts();
@@ -82,15 +129,39 @@ export const ThreadsId: FC = memo(() => {
   return (
     <>
       <Heading as="h2">{threadTitle}</Heading>
-      <Card.Root marginTop="4" padding="12">
-        {posts.length > 0 ? (
-          posts.map((post) => <p className={postStyles}>{post.post}</p>)
-        ) : (
-          <p>投稿されていません。</p>
-        )}
-      </Card.Root>
-      <Stack marginTop="8" className={linkStyles}>
-        <Link to={"/"}>TOPに戻る →</Link>
+      <Stack marginTop="4" gap="8" flexDirection="row">
+        <Stack width="9/12">
+          <Card.Root padding="12">
+            {posts.length > 0 ? (
+              posts.map((post) => <p className={postStyles}>{post.post}</p>)
+            ) : (
+              <p>投稿されていません。</p>
+            )}
+          </Card.Root>
+          <Stack marginTop="8" className={linkStyles}>
+            <Link to={"/"}>TOPに戻る →</Link>
+          </Stack>
+        </Stack>
+        <Stack width="3/12">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack gap="4" flexDirection="column">
+              <Textarea
+                row={10}
+                className={textareaStyles}
+                placeholder="投稿しよう！"
+                {...register("post_text", {
+                  required: "入力してください。",
+                })}
+              />
+              <Button type="submit" loading={isLoading}>
+                投稿
+              </Button>
+            </Stack>
+            {errors.post_text?.message && (
+              <p className={errorMessageStyles}>{errors.post_text?.message}</p>
+            )}
+          </form>
+        </Stack>
       </Stack>
     </>
   );
