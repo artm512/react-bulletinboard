901f5a16729eee5470e635c8b456f41d1e2b4eee
artm512 <44886239+artm512@users.noreply.github.com>
2024-11-11 16:05:58 +0900

    feat: create threads/new page

--- src/pages/threads/new/index.tsx
+++ src/pages/threads/new/index.tsx
@@ -1,1 +1,42 @@
-export const ThreadsNew = () => <>ThreadsNew</>;
+import { FC, memo } from "react";
+import { Link } from "react-router-dom";
+
+// TODO: styled-systemへの参照をエイリアス化したい
+import { css } from "../../../../styled-system/css";
+import { Stack } from "../../../../styled-system/jsx";
+import { Heading } from "../../../components/ui/heading";
+import { Input } from "../../../components/ui/input";
+import { Button } from "../../../components/ui/button";
+
+// TODO: UIコンポーネント化（できればParkUIのLinkコンポーネントかませたい）
+const linkStyles = css({
+  width: "fit-content",
+  _hover: {
+    textDecoration: "underline",
+  },
+});
+
+export const ThreadsNew: FC = memo(() => {
+  const onSubmit = () => {
+    console.log("submit");
+  };
+
+  return (
+    <>
+      <Heading as="h2">スレッド新規作成</Heading>
+      <Stack gap="1.5" maxWidth="lg" marginTop="4">
+        <form>
+          <Stack gap="1.5" flexDirection="row">
+            <Input id="name" placeholder="threads name" />
+            <Button type="button" onClick={onSubmit}>
+              作成
+            </Button>
+          </Stack>
+        </form>
+      </Stack>
+      <Stack marginTop="8" className={linkStyles}>
+        <Link to={"/"}>TOPに戻る →</Link>
+      </Stack>
+    </>
+  );
+});
